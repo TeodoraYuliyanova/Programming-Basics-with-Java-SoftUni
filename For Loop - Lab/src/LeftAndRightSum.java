53ebcd1a2aa117ed1c114b9cf481640edce72cc7
Teodora Yuliyanova <104319243+TeodoraYuliyanova@users.noreply.github.com>
2022-05-05 22:43:46 +0100

    Add files via upload

---
+++ For Loop - Lab/src/LeftAndRightSum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LeftAndRightSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 1; i <= n ; i++) {
+            int number1 = Integer.parseInt(scanner.nextLine());
+         leftSum = leftSum + number1;
+        }
+
+        for (int i = 1; i <= n ; i++) {
+            int number2 = Integer.parseInt(scanner.nextLine());
+          rightSum = rightSum + number2;
+        }
+
+        if (leftSum == rightSum){
+            System.out.println("Yes, sum = " + leftSum);
+        }
+        else {
+            int diff = Math.abs(leftSum - rightSum);
+            System.out.println("No, diff = " + diff);
+        }
+
+    }
+}
