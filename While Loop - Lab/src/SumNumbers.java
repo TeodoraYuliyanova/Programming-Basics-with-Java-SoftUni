0a0a66cef09c856fab81ec6e57d0dae8e98ad99c
Teodora Yuliyanova <104319243+TeodoraYuliyanova@users.noreply.github.com>
2022-05-13 22:00:22 +0100

    Add files via upload

---
+++ While Loop - Lab/src/SumNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+
+        while (sum < number){
+            int nums = Integer.parseInt(scanner.nextLine());
+            sum += nums;
+        }
+        System.out.println(sum);
+
+
+
+
+
+    }
+}
