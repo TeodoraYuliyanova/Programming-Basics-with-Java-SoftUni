354504cd11b31a75a38358dea22bd825d9202072
Teodora Yuliyanova <104319243+TeodoraYuliyanova@users.noreply.github.com>
2022-05-27 21:33:40 +0100

    Add files via upload

---
+++ exam/src/Workout.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Workout {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+       double m = Double.parseDouble(scanner.nextLine());
+       int percent = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= 1000; i++) {
+
+        }
+
+
+
+
+
+
+    }
+}
