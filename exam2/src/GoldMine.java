d70f1157cf2ac3b0e7a48d312286aee07b760082
Teodora Yuliyanova <104319243+TeodoraYuliyanova@users.noreply.github.com>
2022-05-30 16:36:03 +0100

    Add files via upload

---
+++ exam2/src/GoldMine.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GoldMine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberLocations = Integer.parseInt(scanner.nextLine());
+
+    }
+}
